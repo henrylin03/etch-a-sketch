06c287fd4e104b61746665ead61a4f55b319545d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-23 09:08:26 +1100

    create row of 16 empty divs, then cloned 15 more times
    use deep copy (true argument in .cloneNode) to ensure every child is also copied in

---
+++ script.js
@@ -0,0 +1,23 @@
+const gridContainer = document.querySelector(".container");
+const gridRow = gridContainer.querySelector(".row");
+
+function createGrid() {
+    let squareCount = 1;
+    while (squareCount <= 16) {
+        const squareDiv = document.createElement("div");
+        gridRow.appendChild(squareDiv);
+        squareCount++;
+    };
+
+    // initially, the _2nd_ row will be created, as there is already one present created in the above loop
+    let rowCount = 2;
+    while (rowCount <= 16) {
+        const rowClone = gridRow.cloneNode(true);
+        gridContainer.appendChild(rowClone);
+        rowCount++;
+    }
+};
+
+
+
+createGrid();
